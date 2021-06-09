d2b233f9df84fb449a2d445f2689ccebeec77c67
Haider awan <haid3rawan@icloud.com>
2021-06-09 16:30:16 +0500

    Api flow setup

--- Waste2x/Classes/Controllers/Login/LoginViewController.swift
+++ Waste2x/Classes/Controllers/Login/LoginViewController.swift
@@ -52,10 +52,6 @@ class LoginViewController: BaseViewController {
         }
     }
     }
-    
-
-
-
 extension LoginViewController : UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
